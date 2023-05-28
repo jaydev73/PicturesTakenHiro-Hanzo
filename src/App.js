31d833f3a122c9ed324a9c6f48bf0d33d61f8b12
Jay Hernandez <butchich73@gmail.com>
2023-05-28 13:46:59 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState} from 'react'
+// import { getImages } from './api';
 import images from "./api_mock.json"
 import './App.css'
 
 const App = () => {
   const [imageList, setImageList] = useState(images.resources);
+  // const [imageList2, setImageList2] = useState([]);
+  // const handleGetImages = async() => {
+  //   const images = await getImages()
+  //   console.log(images)
+  // }
+  // useEffect(() => {
+  //  handleGetImages()
+  
+    
+  // }, [])
+  
   return <div className="image-grid">
-      {imageList.map((image) => <img src={image.url} alt={image.public_id}></img>)}
+      {imageList.map((image) => <img key={image.asset_id} src={image.url} alt={image.public_id}></img>)}
     </div>
 
   
