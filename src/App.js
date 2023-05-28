93eb16b08e5607aa2a77d7c572f5ebe3ed28eb3f
Jay Hernandez <butchich73@gmail.com>
2023-05-27 23:37:13 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { useState } from 'react'
 import images from "./api_mock.json"
-import "./App.css";
+import './App.css'
+
 const App = () => {
   const [imageList, setImageList] = useState(images.resources);
-  return (
-    <div className="image-grid">
-      {imageList.map((image) => (
-        <img src={image.url} alt={image.public_id}></img>
-      ))}
+  return <div className="image-grid">
+      {imageList.map((image) => <img src={image.url} alt={image.public_id}></img>)}
     </div>
-  );
+
   
 };
 
