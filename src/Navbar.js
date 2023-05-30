6f081a9909540bd28c15a1706a086e3bf9cea104
Jay Hernandez <butchich73@gmail.com>
2023-05-29 18:41:01 -0700

    updated

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
   <svg 
   
   width="615px" height="64px">
- <text kerning="auto" font-family="Myriad Pro" fill="rgb(0, 0, 0)" font-size="72px" x="0px" y="55.359px"><tspan font-size="72px" font-family="Alphabet Number Lore Font V3" fill="#FCF9F9">HIRO&#32;&#32;</tspan><tspan font-size="72px" fill="#FCF9F9">&#38;</tspan><tspan font-size="72px" font-family="Alphabet Number Lore Font V3" fill="#FCF9F9">&#32;&#32;HANZO</tspan></text>
+ <text kerning="auto" font-family="Myriad Pro" fill="rgb(0, 0, 0)" font-size="72px" x="0px" y="55.359px"><tspan font-size="60px" font-family="Alphabet Number Lore Font V3" fill="#FCF9F9">HIRO&#32;&#32;</tspan><tspan font-size="72px" fill="#FCF9F9">&#38;</tspan><tspan font-size="60px" font-family="Alphabet Number Lore Font V3" fill="#FCF9F9">&#32;&#32;HANZO</tspan></text>
  </svg> 
 </div>
     </>
