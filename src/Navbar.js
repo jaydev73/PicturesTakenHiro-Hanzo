db56d4d0dc6804f274da4272d7d6352f5b00ee70
Jay Hernandez <butchich73@gmail.com>
2023-05-29 19:52:08 -0700

    updated

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,13 +3,13 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-        <div className="container">
+        <div class="container">
   {/* <h1>Hiro and Hanzo</h1> */}
   <svg 
   
   width="350px" height="64px">
- <text kerning="auto" fontFamily="Myriad Pro" fill="rgb(0, 0, 0)" fontSize="48px" x="0px" y="55.359px">
- <tspan fontSize="40px" fontFamily="Alphabet Number Lore Font V3" fill="#FCF9F9">HIRO & HANZO</tspan>
+ <text kerning="auto" font-family="Myriad Pro" fill="rgb(0, 0, 0)" font-size="48px" x="0px" y="55.359px">
+ <tspan font-size="40px" font-family="Alphabet Number Lore Font V3" fill="#FCF9F9">HIRO & HANZO</tspan>
  
  </text>
  </svg> 
